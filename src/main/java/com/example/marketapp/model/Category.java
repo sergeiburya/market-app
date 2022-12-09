02e918d505e4743508a5d54fddb6c99612968acf
Serhii Buria <sergeiburya@gmail.com>
2022-12-09 17:20:30 +0200

    added model Product and Category

---
+++ src/main/java/com/example/marketapp/model/Category.java
@@ -0,0 +1,20 @@
+package com.example.marketapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/example/marketapp/model/Product.java
@@ -0,0 +1,21 @@
+package com.example.marketapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    @ManyToOne
+    private Category category;
+}
