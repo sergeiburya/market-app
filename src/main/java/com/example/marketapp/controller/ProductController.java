82c5988211fe6f03d5294c0bef0362895e555152
Serhii Buria <sergeiburya@gmail.com>
2022-12-10 10:28:16 +0200

    udate controllers

--- src/main/java/com/example/marketapp/controller/ProductController.java
+++ src/main/java/com/example/marketapp/controller/ProductController.java
@@ -5,12 +5,19 @@ import com.example.marketapp.dto.ProductResponseDto;
 import com.example.marketapp.dto.mapper.ProductMapper;
 import com.example.marketapp.model.Product;
 import com.example.marketapp.service.ProductService;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
@@ -31,7 +38,7 @@ public class ProductController {
     }
 
     @DeleteMapping("/{id}")
-    public  void deleteById(@PathVariable Long id) {
+    public void deleteById(@PathVariable Long id) {
         productService.deleteById(id);
     }
 
